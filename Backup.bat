ad3cfe7ac22e49be00f95a5d28bd02794043b66f
antoniorb1913 <136896845+antoniorb1913@users.noreply.github.com>
2025-05-01 14:59:47 +0200

    Delete Backup.bat

--- Backup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set FECHA=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%%TIME:~3,2%
-set FECHA=%FECHA: =0%
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -u root -poVP7zsaP skynet > "C:\proyecto\backup\backup_%FECHA%.sql"
