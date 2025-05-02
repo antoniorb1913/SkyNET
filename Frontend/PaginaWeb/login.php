04f2a408f7b752a51190b78ac2894da42eba4007
Josegrcia02 <josefol881@gmail.com>
2025-05-02 13:15:10 +0200

    Cambios realizados para el funcionamiento de estilos y scripts

--- Frontend/PaginaWeb/login.php
+++ Frontend/PaginaWeb/login.php
@@ -95,7 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - SkyNet</title>
-    <link rel="stylesheet" href="login.css" />
+    <link rel="stylesheet" href="Estilos/login.css" />
 </head>
 <body>
     <!-- Barra superior -->
