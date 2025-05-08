94d618a4c4f4f48f1802853bb81bd74d4f10adfe
Josegrcia02 <josefol881@gmail.com>
2025-05-08 20:22:22 +0200

    logo

--- Frontend/PaginaWeb/producto.php
+++ Frontend/PaginaWeb/producto.php
@@ -71,7 +71,7 @@ return '?' . http_build_query($params);
 <body>
     <header>
         <div style="width: 90px; height: auto; padding: 5px; box-sizing: border-box;">
-        <img src="../LOGO/Logo sin fondo.png" alt="Logo" style="width: 100%; height: auto; display: block; max-width: 100%; border: 0;">
+        <img class="logo"src="../LOGO/Logo sin fondo.png" alt="Logo">
         </div>
         <nav>
             <a href="index.php">Inicio</a>
