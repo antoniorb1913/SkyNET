ff6450428e58331293291bb852d5a608e0243e02
antoniorb1913 <antoniorb1913@gmail.com>
2025-05-10 03:57:21 +0200

    usuario admin

--- generar_hash.php
+++ generar_hash.php
@@ -3,7 +3,7 @@ $contraseña = '@dminSkN'; // Contraseña original
 $hash = password_hash($contraseña, PASSWORD_BCRYPT); // Encripta la contraseña
 echo $hash; // Muestra el hash generado para insertarlo en la base de datos
 
-/* Luego meteis el usuario admin donde sale simbolos y numeros y letras ya es lo que teneis
+/* Luego meteis el usuario admin donde la contraseña sale simbolos y numeros y letras ahi es lo que teneis
 que poner lo que ha generado este php que es la contraseña @dminSkN encriptada, luego para 
 iniciar sesion usuario admin contraseña @dminSkN
 
