2c2889764a1ff5ede7996c4dfaf284d584f7fba0
antoniorb1913 <antoniorb1913@gmail.com>
2025-05-10 15:50:34 +0200

    organizacion de estilos en el backend

--- Backend/login.php
+++ Backend/login.php
@@ -37,77 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login | SkyNET</title>
-    <style>
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f5f5f5;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 80vh;
-        margin: 0;
-    }
-
-    .logo {
-        margin-bottom: 20px; /* Separación entre el logo y el login */
-        text-align: center;
-    }
-
-    .logo img {
-        height: 210px; /* Ajusta el tamaño del logo */
-        width: 250px;
-    }
-
-    .login-container {
-        background: rgb(255, 255, 255);
-        padding: 20px;
-        border-radius: 10px;
-        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
-        text-align: center;
-        color: rgb(0, 0, 0);
-        width: 300px;
-    }
-
-    .input-group {
-        margin-bottom: 15px;
-    }
-
-    .input-group label {
-        display: block;
-        text-align: left;
-        font-size: 14px;
-        margin-bottom: 5px;
-    }
-
-    .input-group input {
-        width: 90%;
-        padding: 10px;
-        border: 1px solid black;
-        border-radius: 5px;
-        color: rgb(0, 0, 0);
-    }
-
-    .btn {
-        width: 100%;
-        background-color: #007bff;
-        color: white;
-        border: none;
-        padding: 10px;
-        cursor: pointer;
-        border-radius: 5px;
-        font-size: 16px;
-    }
-
-    .btn:hover {
-        background-color: #0056b3;
-    }
-
-    .error {
-        color: #ff4d4d;
-        margin-bottom: 10px;
-    }
-    </style>
+    <link rel="stylesheet" href="Estilos/login.css">
 </head>
 <body>
     <div class="logo">
