8902f4632453c80aa9055b2394c3d2a51cd2fd60
antoniorb1913 <antoniorb1913@gmail.com>
2025-05-10 03:56:22 +0200

    usuario admin

--- generar_hash.php
+++ generar_hash.php
@@ -1,10 +1,16 @@
 <?php
-$contraseña_ingresada = '@dminSkN';
-$hash_guardado = '$2y$10$vZEYPQ/nb.5DHe2fA96Dr.0sn7JugubiKmtzv9Jcm7aJzYxBj3Ji.'; // Copia el hash exacto de la base de datos
+$contraseña = '@dminSkN'; // Contraseña original
+$hash = password_hash($contraseña, PASSWORD_BCRYPT); // Encripta la contraseña
+echo $hash; // Muestra el hash generado para insertarlo en la base de datos
 
-if (password_verify($contraseña_ingresada, $hash_guardado)) {
-    echo "¡Contraseña verificada correctamente!";
-} else {
-    echo "Error: La contraseña no coincide.";
-}
+/* Luego meteis el usuario admin donde sale simbolos y numeros y letras ya es lo que teneis
+que poner lo que ha generado este php que es la contraseña @dminSkN encriptada, luego para 
+iniciar sesion usuario admin contraseña @dminSkN
+
+INSERT INTO admin (nombre, email, contrasena, created_at) 
+VALUES ('admin', 'admin@skynet.com', '$2y$10$OHMD0hhgo36Ky9VyAWD2C.GvVCNowtm6JnALe3hZIt2ZNsZitICya', NOW());*/
 ?>
+
+
+
+
